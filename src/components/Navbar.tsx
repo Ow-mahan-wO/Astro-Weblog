933cd19359eece966b9e54058e63a4d222d6557a
Ow-mahan-wO <112275799+Ow-mahan-wO@users.noreply.github.com>
2024-03-23 01:25:02 +0430

    responsive pages latoyts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import Button from "../ui/Button";
 
 import navData from "../data/nav-data";
+import HamburgerMenu from "./HamburgerMenu";
 
 const Navbar = () => {
   return (
     <>
       <header>
         <nav className="flex justify-between items-center p-6">
-          <div>
+          <div className="md:hidden">
+            <HamburgerMenu />
+          </div>
+          <div className="md:flex hidden">
             <Button isOutLine={false} onClick={() => {}}>
               {" "}
               Login{" "}
@@ -17,7 +21,7 @@ const Navbar = () => {
               SignUp{" "}
             </Button>
           </div>
-          <div className="list-none flex">
+          <div className="list-none flex hidden md:flex">
             {navData.map((item, i) => (
               <div key={i}>
                 <a className="m-2 text-blue-500 p-3" href={item.path}>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      'xl':'1200px',
+      'lg': "1085px",
+      'md': "800px",
+      'sm': "600px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
